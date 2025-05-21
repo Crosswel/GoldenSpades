8afd1507ded865f5be10b3c64e55cb01f6714325
Crosswel <joaomarcruz@gmail.com>
2025-05-21 16:39:01 +0100

    21

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'eb4f7c4147ddadb6d2f8cbda19dda029464cdb51',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -458,9 +458,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'eb4f7c4147ddadb6d2f8cbda19dda029464cdb51',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
